3a4a4afb11ee9e0ab4f40c126efacf1f990d57fc
nehemiahh23 <nehemiahelihenry@gmail.com>
2026-01-09 17:25:19 -0500

    be done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
+
+  let val = central(id)
+  val = val.toString()
+  const val1 = await db1(id)
+  
+  //   console.log(val)
+  //   console.log(val1)
+  return val
 }
 
-console.log(central(2))
+console.log(await getUserData(1))
